5d307badce90e2f36081528535cebd1ebdbcd960
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2025-02-26 04:47:35 +0700

    add POS for SOAL PRAKTIKUM

---
+++ POS/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h1>Selamat Datang di Website POS</h1>
+</body>
+</html>
---
+++ POS/resources/views/sales/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Halaman Transaksi</title>
+</head>
+<body>
+    <h1>Halaman Transaksi POS</h1>
+</body>
+</html>
