17d993d988864f432ffb61649ce0c3dbd0198515
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2025-03-04 08:58:59 +0700

    add Minggu_3 (Praktikum-1)

---
+++ Minggu_3/PWL_POS/app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show($id, $name)
+    {
+        return view('user.profile', compact('id', 'name'));
+    }
+    public function profile($id)
+    {
+        // Ambil data user berdasarkan ID
+        $user = User::find($id);
+
+        // Periksa apakah user ditemukan
+        if (!$user) {
+            return abort(404, 'User tidak ditemukan');
+        }
+
+        return view('user.profile', compact('user'));
+    }
+
+}
+
+
+
+    // public function profile()
+    // {
+    //     $user = Auth::user(); // Mendapatkan user yang sedang login
+
+    //     return view('user.profile', compact('user')); // Kirim ke view
+    // }
