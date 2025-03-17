25cb931a33152c72e7c52e811e1c3180a8b7b366
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2025-03-17 13:28:50 +0700

    add  Minggu_6

---
+++ Minggu_6/PWL_POS/app/Http/Controllers/KategoriController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\DataTables\KategoriDataTable;
+use App\Models\KategoriModel;
+
+class KategoriController extends Controller
+{
+    // public function index()
+    // {
+    //     /* $data = [
+    //         'kategori_kode' => 'SNK',
+    //         'kategori_nama' => 'Snack/Makanan Ringan',
+    //         'created_at' => now()
+    //     ];
+    //     DB::table('m_kategori')->insert($data);
+    //     return 'Insert data baru berhasil'; */
+
+    //     // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'camilan']);
+    //     // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
+
+    //     // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
+    //     // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row . ' baris';
+
+    //     // $data = DB::table('m_kategori')->get();
+    //     // return view('kategori', ['data' => $data]);
+
+    // }
+
+
+    // ---------------------------------------------
+    // jobsheet 5
+    // ---------------------------------------------
+    public function index(KategoriDataTable $dataTable)
+    {
+        return $dataTable->render('kategori.index');
+    }
+    public function create()
+    {
+        return view('kategori.create');
+    }
+
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+
+        return redirect('/kategori');
+    }
+
+
+
+
+
+    //Tugas nomer 3 - Menambahkan Edit
+    public function edit($id)
+    {
+        $kategori = KategoriModel::findOrFail($id);
+        return view('kategori.edit', compact('kategori'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'kategori_kode' => 'required|string|max:50',
+            'kategori_nama' => 'required|string|max:100',
+        ]);
+
+        $kategori = KategoriModel::findOrFail($id);
+        $kategori->update([
+            'kategori_kode' => $request->kategori_kode,
+            'kategori_nama' => $request->kategori_nama,
+        ]);
+
+        return redirect()->route('kategori.index')->with('success', 'Kategori berhasil diperbarui!');
+    }
+
+
+
+
+    //Tugas nomer 3 - Menambahkan Edit
+    public function delete($id)
+    {
+        KategoriModel::where('kategori_id', $id)->delete();
+        return redirect(to: '/kategori');
+    }
+
+
+
+}
