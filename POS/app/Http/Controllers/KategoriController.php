49be581c395a2265b16aeb78abcb2f5c6276ff5c
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2025-04-13 12:39:10 +0700

    IMPLEMENTASI JS-03 PRAKTIKUM 5: Implementasi CRUD Menggunakan QUERY Builder ✨

--- POS/app/Http/Controllers/KategoriController.php
+++ POS/app/Http/Controllers/KategoriController.php
@@ -7,21 +7,23 @@ use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-  public function index()
-  {
-    // ============================
-    // | JOBSHEET 3 - PRAKTIKUM 4 |
-    // ============================
-    // DB::insert('insert into m_kategori(kategori_kode, kategori_nama, created_at) values(?, ?, ?)', ['BPB', 'Barang Pecah Belah', now()]);
-    // return 'Insert data baru berhasil';
+// implementasi pos jobsheet 3
+    public function index() {
+        // $data = [
+        //     'kategori_kode' => 'SNK',
+        //     'kategori_nama' => 'Snack/Makanan Ringan',
+        //     'created_at' => now()
+        // ];
+        // DB::table('m_kategori')->insert($data);
+        // return 'Insert data baru berhasil';
 
-    // $row = DB::update('update m_kategori set kategori_nama = ? where kategori_kode = ?', ['Pecah Belah', 'BPB']);
-    // return 'Update data berhasil, jumlah data yang diupdate: '.$row. ' baris';
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
+        // return "Update data berhasil. Jumlah data yang di update: " . $row.' baris';
 
-    // $row = DB::delete('delete from m_kategori where kategori_kode = ?', ['BPB']);
-    // return 'Delete data berhasil, jumlah data yang dihapus: '.$row. ' baris';
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
+        // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row.' baris';
 
-    $data = DB::select('select * from m_kategori');
-    return view('kategori', ['data' => $data]);
-  }
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
+    }
 }
