5a16fd7e7e72e93489ec0200de3fc197b91a6ef4
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2025-04-13 18:48:26 +0700

    IMPLEMENTASI JS-05 PRAKTIKUM 3: Implementasi jQuery Datatable di AdminLTE ✨

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -45,9 +45,6 @@ use App\Http\Controllers\PenjualanDetailController;
 //     return view('welcome');
 // });
 
-
-Route::get('/', [WelcomeController::class, 'index']);
-
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
@@ -57,15 +54,32 @@ Route::get('/stok', [StokController::class, 'index']);
 Route::get('/penjualan', [PenjualanController::class, 'index']);
 Route::get('/penjualan_detail', [PenjualanDetailController::class, 'index']);
 
+
+// Praktikum 2 - Langkah 5
+Route::get('/', [WelcomeController::class, 'index']);
+
+// Praktikum 3 - Langkah 3 &  JS6 - Praktikum 1 - Langkah 6
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']); // Menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']); // Menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']); // Menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']); // Menyimpan data user baru
+    Route::get('/create_ajax', [UserController::class, 'create_ajax']); // Menampilkan halaman form tambah user Ajax
+    Route::post('/ajax', [UserController::class, 'store_ajax']); // Menyimpan data user baru Ajax
+    Route::get('/{id}', [UserController::class, 'show']); // Menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']); // Menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']); // Menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); // Menghapus data user
+});
 //===================================
 //| JOBSHEET 4 - PRAKTIKUM 2.6 CRUD |
 //===================================
 // User
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
+// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
 // Level
 Route::get('/level/tambah', [LevelController::class, 'tambah']);
