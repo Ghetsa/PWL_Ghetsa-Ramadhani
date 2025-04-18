35f323989a814b9d94b6636f44522edc1db62111
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2025-04-19 06:22:11 +0700

    IMPLEMENTASI JS-07 PRAKTIKUM 2 & 3: Implementasi Authorizaton dan  Multi-Level Authorizaton di Laravel dengan Middleware ✨

--- POS/app/Models/UserModel.php
+++ POS/app/Models/UserModel.php
@@ -21,15 +21,32 @@ class UserModel extends Authenticatable
    protected $hidden = ['password'];
    protected $casts = ['password' => 'hashed'];
 
-   // ============================
-   // | JOBSHEET 4 - PRAKTIKUM 2 |
-   // ============================
-   //  protected $fillable = ['level_id', 'username', 'nama'];
-    protected $fillable = ['username', 'nama', 'password', 'level_id', 'created_at', 'updated_at'];
+    // --------------------------------------
+    // Jobsheet 7 Praktikum 2 langkah 1
+    // --------------------------------------
+    // Relasi ke tabel level
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 
-   // Relasi ke tabel level
-   public function level(): BelongsTo
-   {
-      return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-   }
+    // Mendapatkan nama role
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+
+    // Cek apakah user memiliki role tertentu
+    public function hasRole($role): bool
+    {
+        // dd($this->level->attributes); // Cek semua atribut yang dimiliki LevelModel
+        return $this->level->level_kode == $role;
+    }
+
+    
+    // Mendapatkan kode role
+    public function getRole()
+    {
+        return $this->level->level_kode;
+    }
 }
