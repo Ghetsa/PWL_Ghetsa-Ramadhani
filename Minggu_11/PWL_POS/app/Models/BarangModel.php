503877c8627aa83e3087e70dd9e48e6a2e41a526
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2025-05-05 21:23:22 +0700

    add: Minggu_11 Tugas

--- Minggu_11/PWL_POS/app/Models/BarangModel.php
+++ Minggu_11/PWL_POS/app/Models/BarangModel.php
@@ -5,18 +5,36 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BarangModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_barang';
-    protected $primaryKey = 'barang_id';
+    protected $table = 'm_barang'; // Nama tabel di database
+    protected $primaryKey = 'barang_id'; // Primary key tabel
 
-    protected $fillable = ['kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual'];
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array
+     */
+    protected $fillable = ['kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual', 'image'];
 
+    public $timestamps = true;
+
+    /**
+     * Relasi ke tabel kategori.
+     * Barang memiliki satu kategori.
+     */
     public function kategori(): BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
     }
 }
