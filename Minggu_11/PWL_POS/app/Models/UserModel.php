3a041d537c8ebcc1fff7ee69d173ceeae5e8ed0b
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2025-05-05 19:39:32 +0700

    add: Minggu_11/ Praktikum 1

--- Minggu_11/PWL_POS/app/Models/UserModel.php
+++ Minggu_11/PWL_POS/app/Models/UserModel.php
@@ -1,63 +1,43 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+<?php 
+ 
+namespace App\Models; 
+ 
+use Illuminate\Database\Eloquent\Model; 
+use Tymon\JWTAuth\Contracts\JWTSubject; 
+use Illuminate\Database\Eloquent\Casts\Attribute; 
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable implements JWTSubject
-{
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
-
-    protected $table = 'm_user'; // mendifinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; // mendefinisikan primary key dari tabel yang digunakan
-    protected $fillable = ['username', 'nama', 'password', 'level_id', 'foto'];
-
-    protected $hidden = ['password'];
-
-    protected $casts = ['password' => 'hashed'];
-    /**
-     * The attributes that are mass assignable.
-     * 
-     * @var array
-     */
-
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
-    /**
-     * Mendapatkan nama role
-     */
-    public function getRoleName(): string
-    {
-        return $this->level->level_nama;
-    }
-
-    /**
-     * Cek apakah user memiliki role tertentu
-     */
-    public function hasRole($role): bool
-    {
-        return $this->level->level_kode == $role;
-    }
-
-    /**
-     * Mendapatkan kode role
-     */
-    public function getRole()
-    {
-        return $this->level->level_kode;
-    }
-}
+class UserModel extends Authenticatable implements JWTSubject 
+{ 
+ 
+    public function getJWTIdentifier(){ 
+        return $this->getKey(); 
+    } 
+ 
+    public function getJWTCustomClaims(){ 
+        return []; 
+    } 
+ 
+    protected $table = 'm_user'; 
+    protected $primaryKey = 'user_id'; 
+ 
+    protected $fillable = [ 
+        'username', 
+        'nama', 
+        'password', 
+        'level_id', 
+        'image'//tambahan 
+    ]; 
+ 
+    public function level() 
+    { 
+        return $this->belongsTo(LevelModel::class, 'level_id', 
+'level_id'); 
+    } 
+    protected function image(): Attribute 
+    { 
+        return Attribute::make( 
+            get: fn ($image) => url('/storage/posts/' . $image), 
+        ); 
+    } 
+} 
